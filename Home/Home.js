58b903a6ad2c41b724ecc9afa9d7a9b9a1359cde
DauThiHoa <91518695+DauThiHoa@users.noreply.github.com>
2021-12-28 11:54:37 +0700

    HomePage

--- Home/Home.js
+++ Home/Home.js
@@ -22,13 +22,34 @@ function right_SPM (SPM){
 function hot1 (hot1){
     document.querySelector('.DoDungVanPhong').style.display =  "none"  ;
     document.querySelector('.NoiThatGo').style.display = "none"  ;
+    document.getElementById('hot1').style.color = "#0099aa"  ;
+    document.getElementById('hot2').style.color = "black"  ;
+    document.getElementById('hot3').style.color = "black"  ;
 }
 function hot2 (hot2){
     document.querySelector('.DoDungVanPhong').style.display = "block"  ;
     document.querySelector('.NoiThatGo').style.display = "none"  ;
+    document.getElementById('hot1').style.color = "black"  ;
+    document.getElementById('hot2').style.color = "#0099aa"  ;
+    document.getElementById('hot3').style.color = "black"  ;
 }
 function hot3 (hot3){
     document.querySelector('.DoDungVanPhong').style.display =  "none"  ;
     document.querySelector('.NoiThatGo').style.display = "block"  ;
+    document.getElementById('hot1').style.color = "black"  ;
+    document.getElementById('hot2').style.color = "black"  ;
+    document.getElementById('hot3').style.color = "#0099aa"  ;
+}
+function leftBoSuuTap (leftBoSuuTap){
+    document.querySelector('.tenCacSanPham2').style.display = "none";
+}
+function rightBoSuuTap (rightBoSuuTap){
+    document.querySelector('.tenCacSanPham2').style.display = "block"  ;
 }
 
+function leftSanPhamKhuyenMai (leftSanPhamKhuyenMai){
+    document.querySelector('.tenCacSanPham4').style.display = "none";
+}
+function rightSanPhamKhuyenMai (rightSanPhamKhuyenMai){
+    document.querySelector('.tenCacSanPham4').style.display = "block"  ;
+}
